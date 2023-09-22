27ff5757726409f9483ecf022ae29c3d89b66baf
UUoocl <99063397+UUoocl@users.noreply.github.com>
2023-09-22 14:46:40 -0400

    Update preload.js
    adding camera window

--- preload.js
+++ preload.js
@@ -3,5 +3,17 @@ var windowId;
 
 contextBridge.exposeInMainWorld('electronAPI', {
   slideWindow: (IP, Port, PW, Link) => ipcRenderer.send('open-slide-window', IP, Port, PW, Link),
-  getSourceId: () => ipcRenderer.send('get-source')
+  cameraWindow: (CameraID) => ipcRenderer.send('open-camera-window', CameraID),
+  getCameraId: () => ipcRenderer.send('get-cameras')
+})
+
+
+ipcRenderer.on('SET_CAMERA_SOURCE', async (event, deviceName, deviceID) => { 
+  console.log(deviceID)
+  var x = document.getElementById("cameras");
+  var option = document.createElement("option");
+  option.text = deviceName;
+  option.id = deviceID;
+  x.add(option);
+  window.document.body.insertBefore(windowId, window.document.body.firstChild);
 })
